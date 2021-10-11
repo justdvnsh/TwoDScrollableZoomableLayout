c2e999b4b8741de5975aee7870f3cf69f887bab3
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-10-11 15:23:56 +0530

    initial commit

---
+++ app/src/main/java/divyansh/tech/twodscrollablezoomable/MainActivity.java
@@ -0,0 +1,14 @@
+package divyansh.tech.twodscrollablezoomable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ twoDScrollableZoomableLibrary/src/main/java/divyansh/tech/twodscrollablezoomablelibrary/TwoDScrollableZoomableView.java
@@ -0,0 +1,4 @@
+package divyansh.tech.twodscrollablezoomablelibrary;
+
+public class TwoDScrollableZoomableView {
+}
